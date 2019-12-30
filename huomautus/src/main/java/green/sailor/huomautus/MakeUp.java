ce33875247fdc1edc8896fe46985ac6ef2225e48
Laura F Dickinson <l@veriny.tf>
2019-12-30 02:36:13 +0000

    Add fake mod for game crashing

---
+++ huomautus/src/main/java/green/sailor/huomautus/MakeUp.java
@@ -0,0 +1,7 @@
+package green.sailor.huomautus;
+
+public class MakeUp {
+    public void onInitialize() {
+        throw new Error("Huomautus should be included via compileOnly, not api/implementation");
+    }
+}
